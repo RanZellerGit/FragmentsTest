bd0609b83601fcaaa85f42416e95737263c32ea1
Ran Zeller <ranz@gett.com>
2019-03-05 09:43:37 +0200

    first commit

---
+++ app/src/main/java/com/example/fragmenttest/fragment1.kt
@@ -0,0 +1,53 @@
+package com.example.fragmenttest
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_fragment1.*
+
+
+private const val ARG_PARAM2 = "param2"
+
+class fragment1 : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("fragment1 onCreate")
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        openFragButton.setOnClickListener {
+            fragmentManager?.beginTransaction()?.add(R.id.container,Fragment2())?.addToBackStack("")?.commitAllowingStateLoss()
+        }
+    }
+
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment fragment1.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = fragment1()
+    }
+}
